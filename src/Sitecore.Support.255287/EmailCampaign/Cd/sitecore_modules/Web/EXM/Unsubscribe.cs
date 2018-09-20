8594db8414b0280c0609e433f33b077b22c894f4
DK\dmsa <dmsa@sitecore.net>
2018-09-20 15:54:54 +0300

    Fix the issue #255287. Original code is not needed since the changes can be found easily.

---
+++ src/Sitecore.Support.255287/EmailCampaign/Cd/sitecore_modules/Web/EXM/Unsubscribe.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.EmailCampaign.Cd.sitecore_modules.Web.EXM
+{
+  using Sitecore.Modules.EmailCampaign.Core;
+  using UnsubscribeOrigin = Sitecore.EmailCampaign.Cd.sitecore_modules.Web.EXM.Unsubscribe;
+
+  public class Unsubscribe : UnsubscribeOrigin
+  {
+    protected override string LanguageName
+    {
+      get
+      {
+        return string.IsNullOrEmpty(ExmContext.Message.TargetLanguage.Name) ? base.LanguageName : ExmContext.Message.TargetLanguage.Name;
+      }
+    }
+  }
+}
